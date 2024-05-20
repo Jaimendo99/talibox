b55770f0a98e551000e67f7b431c33eee4d45913
Jaime Mendoza <jaimendo99@gmail.com>
2024-05-20 06:04:11 -0500

    fixed jwt invalid signature error

--- inits/jwtConfig.go
+++ inits/jwtConfig.go
@@ -2,26 +2,30 @@ package inits
 
 import (
 	"fmt"
-	"github.com/golang-jwt/jwt/v5"
-	echojwt "github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
 	"net/http"
 	"os"
 	"talibox/controllers/Models"
+
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
 )
 
 var Config = echojwt.Config{
 	NewClaimsFunc: func(c echo.Context) jwt.Claims {
+		fmt.Println(os.Getenv("JWT_SECRET"))
 		return &Models.JwtCustomClaims{}
 	},
 
-	SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	SigningKey: []byte("SECRET"),
 
 	TokenLookup: "cookie:jwt",
 
 	ErrorHandler: func(c echo.Context, err error) error {
 		fmt.Printf("JWT Error: %v\n", err) // Log the error for debugging
 
-		return c.Redirect(http.StatusPermanentRedirect, "/login")
+		return c.JSON(http.StatusUnauthorized, map[string]string{
+			"message": "Unauthorized",
+		})
 	},
 }
